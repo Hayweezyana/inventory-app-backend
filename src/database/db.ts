8973740d1888b37e35dcd6ae1aeeaacd706fb13a
Ayotunde Adeboyeje <ayotundeadeboyeje@gmail.com>
2025-07-02 20:46:00 +0100

    render adjustments

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,6 +1,6 @@
 // db.ts or knex.ts
 import knex from 'knex';
-import knexConfig from './src/database/knexfile';
+import knexConfig from './knexfile';
 
 const env = process.env.NODE_ENV || 'development';
 const db = knex(knexConfig[env]);
